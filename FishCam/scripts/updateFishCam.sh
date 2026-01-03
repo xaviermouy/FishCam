5064f46f9c2b4ee8e89b71ff581c6bf074d4f23c
Xavier Mouy <xavier.mouy@gmail.com>
2026-01-03 14:11:15 -0500

    power saving fixes

--- FishCam/scripts/updateFishCam.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/fishcam/Desktop/FishCam
-git fetch origin
-git reset --hard origin/main
-git clean -fd -e data -e logs
--- FishCam/scripts/updateFishCamRepo.sh
+++ FishCam/scripts/updateFishCamRepo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/fishcam/Desktop/FishCam
 git fetch origin
-git reset --hard origin
-git clean -fd
+git reset --hard origin/main
+git clean -fd -e data -e logs
